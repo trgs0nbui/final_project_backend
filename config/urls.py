e4b2a3e498a59762f9c3f5ad8f104ad8a40f2a8b
trgs0nbui <buisonvpvttt@gmail.com>
2026-04-28 10:55:24 +0700

    feature: thêm logic authentication với JWT, xác thực email người dùng

--- config/urls.py
+++ config/urls.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    # path('api/users/', include('apps.users.urls')),
+    path("admin/", admin.site.urls),
+    path("api/", include("apps.users.urls")),
     # path('api/projects/', include('apps.projects.urls')),
     # path('api/tasks/', include('apps.tasks.urls')),
 ]
