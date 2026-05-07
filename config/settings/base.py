f89a9d8a1143002091c01cb273fb5abc018ed9aa
trgs0nbui <buisonvpvttt@gmail.com>
2026-05-07 16:20:09 +0700

    feat: thêm logic, endpoint verify email

--- config/settings/base.py
+++ config/settings/base.py
@@ -180,7 +180,7 @@ EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')  # Gmail App Password (kh
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', EMAIL_HOST_USER)
 
 # URL frontend dùng để tạo link xác thực trong email
-FRONTEND_URL = os.getenv('FRONTEND_URL', 'http://localhost:8000')
+FRONTEND_URL = os.getenv('FRONTEND_URL', 'http://localhost:5173')
 
 # ============= CORS =================
 CORS_ALLOWED_ORIGINS = [
